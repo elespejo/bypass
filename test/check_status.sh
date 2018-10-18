f29bd0928db3a7a2699806e7b56b29c4143252ff
Valerio-Perez <Valerio.Perez@protonmail.com>
2018-10-19 00:10:41 +0800

    Remove old deployment

--- test/check_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CUR_DIR=`cd $(dirname $0);pwd`
-
-docker-compose -f $CUR_DIR/docker-compose.yml ps
-docker-compose -f $CUR_DIR/docker-compose.yml logs
-
-sudo iptables -t nat -S
-sudo ipset list -n
