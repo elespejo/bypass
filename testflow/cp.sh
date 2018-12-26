ec0761c4694380cc69c3da3525b0851e72226e3d
AP1001 <alejandro.perez1001@protonmail.com>
2018-12-26 11:29:29 +0800

    add testflow

---
+++ testflow/cp.sh
@@ -0,0 +1,8 @@
+rm -rf script
+mkdir script
+cp -r ~/github/testflow-generator/output/.env script/
+cp -r ~/github/testflow-generator/output/* script/
+ls -al script
+#git checkout script/.env
+cp -r other_info script
+cp info.yml script
